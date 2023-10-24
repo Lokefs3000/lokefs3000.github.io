3da5f8c0497f4aa0eafcebf3060012f50890f87d
Lokefs3000 <95932279+Lokefs3000@users.noreply.github.com>
2023-10-24 14:48:20 +0200

    y

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
-function transtion(page) {
-    document.getElementById("transition").classList.remove("transition_swipe_off")
-    document.getElementById("transition").classList.add("transition_swipe_on")
-
-    setTimeout(function() {
-        document.location = page
-    }, 400)
+function transport(url) {
+    location.pathname = url + ".html";
 }
